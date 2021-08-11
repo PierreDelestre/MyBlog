977e3bcd00ef1e5bae92fd3a539bf2b5e78ee86a
Pierre Delestre <pdelestre@digital143.onmicrosoft.com>
2021-08-11 09:56:34 +0100

    Add project files.

---
+++ MyBlog.data/Models/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using MyBlog.data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.data.Models
+{
+    public class BlogPost : IMyBlogItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public String Text { get; set; }
+        public DateTime PublishDate { get; set; }
+        public Category Category { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ MyBlog.data/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using MyBlog.data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.data.Models
+{
+    public class Category : IMyBlogItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
